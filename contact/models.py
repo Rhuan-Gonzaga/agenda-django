94c3e93fe5b2a49d9736cade3a57318dac616baf
Rhuan-Gonzaga <rhuangonzaga22@gmail.com>
2025-01-23 13:06:13 -0300

    Criando o campo owner e usando o model USer

--- contact/models.py
+++ contact/models.py
@@ -1,14 +1,24 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
 
 class Contact(models.Model):
+    class Meta:
+        verbose_name = 'Contact'
+        verbose_name_plural = 'Contacts'
+
+
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, blank=True)
     phone = models.CharField(max_length=50)
@@ -18,6 +28,7 @@ class Contact(models.Model):
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True,null=True)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True,null=True)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
