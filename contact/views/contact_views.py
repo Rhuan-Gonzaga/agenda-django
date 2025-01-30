6f13e3c35bcbd31db3237a84b367092662ad80b5
rhuan-gonzaga <rhuangonzaga22@gmail.com>
2025-01-30 19:49:04 -0300

    Criando o template, a view e a url para exebir o contato

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,12 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from contact.models import Contact
+from django.http import Http404
 
 def index(request):
-    contacts = Contact.objects.all()
+    contacts = Contact.objects.filter(show=True).order_by('-id')[0:10]
    
     return render(
         request,
         'contact/index.html',
         {'contacts': contacts}
+    )
+    
+
+def contact(request,contact_id):
+    #single_contact = Contact.objects.filter(pk=contact_id).first()
+    single_contact = get_object_or_404(Contact, pk=contact_id,show=True)
+   
+    return render(
+        request,
+        'contact/contact.html',
+        {'contact': single_contact}
     )
     
