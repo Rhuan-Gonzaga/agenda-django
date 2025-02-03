6654a6e967932f6955f682eea28fa6fca22804fd
Rhuan-Gonzaga <rhuangonzaga22@gmail.com>
2025-02-03 20:43:15 -0300

    criando o header e criando arquivos parciais

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -8,17 +8,17 @@ def index(request):
     return render(
         request,
         'contact/index.html',
-        {'contacts': contacts}
+        {'contacts': contacts,'site_title': 'Contatos - '}
     )
     
 
 def contact(request,contact_id):
     #single_contact = Contact.objects.filter(pk=contact_id).first()
     single_contact = get_object_or_404(Contact, pk=contact_id,show=True)
-   
+
     return render(
         request,
         'contact/contact.html',
-        {'contact': single_contact}
+        {'contact': single_contact, 'site_title': f'{single_contact.first_name} {single_contact.last_name} - '}
     )
     
